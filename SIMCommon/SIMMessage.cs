57425cb29c8d617a969ba6681d74597c49c16506
Sty BL <stysdaman@gmail.com>
2016-09-05 10:15:21 +0300

    Implemented SIMMessage class.

---
+++ SIMCommon/SIMMessage.cs
@@ -0,0 +1,33 @@
+﻿namespace SIMCommon
+{
+    using System;
+    using Newtonsoft.Json;
+
+    public class SIMMessage
+    {
+        public SIMMessage(int senderID, int recipientID, string text)
+        {
+            this.SenderID = senderID;
+            this.RecipientID = recipientID;
+            this.Text = text;
+            this.Timestamp = DateTime.Now;
+        }
+
+        [JsonConstructor]
+        public SIMMessage(int senderID, int recipientID, string text, DateTime timestamp)
+        {
+            this.SenderID = senderID;
+            this.RecipientID = recipientID;
+            this.Text = text;
+            this.Timestamp = timestamp;
+        }
+
+        public int SenderID { get; private set; }
+
+        public int RecipientID { get; private set; }
+
+        public string Text { get; private set; }
+
+        public DateTime Timestamp { get; private set; }
+    }
+}
