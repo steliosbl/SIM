7dec69e9c71f96f31fb7457d6c14602344617bc8
Sty BL <stysdaman@gmail.com>
2016-09-05 10:19:38 +0300

    Added GlobalConstants file.

---
+++ SIMCommon/GlobalConstants.cs
@@ -0,0 +1,9 @@
+﻿namespace SIMCommon
+{
+    public static class GlobalConstants
+    {
+        public const int TcpStreamByteSize = 512;
+        public const int SIMServerPort = 13197;
+        public const int SIMClientPort = 71913;
+    }
+}
