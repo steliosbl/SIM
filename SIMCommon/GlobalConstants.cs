1c3c41a8758ba730ec973c59a8c84eb68ebb47e2
Sty BL <stysdaman@gmail.com>
2016-09-07 11:23:16 +0300

    Implemented SIMServerConfig class.

--- SIMCommon/GlobalConstants.cs
+++ SIMCommon/GlobalConstants.cs
@@ -5,5 +5,6 @@
         public const int TcpStreamByteSize = 512;
         public const int SIMServerPort = 13197;
         public const int SIMClientPort = 71913;
+        public const string SIMAuthDBUserTable = "SIMUSERS";
     }
 }
---
+++ SIMServer/SIMServerConfig.cs
@@ -0,0 +1,17 @@
+﻿namespace SIMServer
+{
+    using System;
+    using Newtonsoft.Json;
+    using SDatabase;
+
+    public class SIMServerConfig
+    {
+        public SDatabase.MySQL.ConnectionData ConnectionInfo { get; private set; }
+        
+        [JsonConstructor]
+        public SIMServerConfig(SDatabase.MySQL.ConnectionData connectionInfo)
+        {
+            this.ConnectionInfo = connectionInfo;
+        }
+    }
+}
