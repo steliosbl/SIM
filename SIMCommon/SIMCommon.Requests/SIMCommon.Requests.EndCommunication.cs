a68a60768ed0d2aaff4f5a51f44bc6a40f0e05a2
Sty BL <stysdaman@gmail.com>
2016-09-07 14:28:40 +0300

    Implemented EndComm and SignOut requests.

---
+++ SIMCommon/SIMCommon.Requests/SIMCommon.Requests.EndCommunication.cs
@@ -0,0 +1,9 @@
+﻿namespace SIMCommon.Requests
+{
+    public sealed class EndCommunication : Base
+    {
+        public EndCommunication() : base()
+        {
+        }
+    }
+}
---
+++ SIMCommon/SIMCommon.Requests/SIMCommon.Requests.SignOut.cs
@@ -0,0 +1,9 @@
+﻿namespace SIMCommon.Requests
+{
+    public sealed class SignOut : Base
+    {
+        public SignOut() : base()
+        {
+        }
+    }
+}
