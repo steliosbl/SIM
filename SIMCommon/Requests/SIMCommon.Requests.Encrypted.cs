ae02f8598effb4684d74a8a6a390cd9d5e3f231e
Sty BL <stysdaman@gmail.com>
2016-09-08 08:50:59 +0300

    Added encrypted request.

---
+++ SIMCommon/Requests/SIMCommon.Requests.Encrypted.cs
@@ -0,0 +1,15 @@
+﻿namespace SIMCommon.Requests
+{
+    public sealed class Encrypted : Base
+    {
+        public Encrypted(byte[] encryptedSessionKey, string encryptedRequest) : base()
+        {
+            this.EncryptedSessionKey = encryptedSessionKey;
+            this.EncryptedRequest = encryptedRequest;
+        }
+
+        public byte[] EncryptedSessionKey { get; private set; }
+
+        public string EncryptedRequest { get; private set; }
+    }
+}
