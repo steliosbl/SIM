41b6aace2a37dc2aafb6afc2e944a1caae97861a
Sty BL <stysdaman@gmail.com>
2016-09-09 10:45:01 +0300

    Implemented backlog class.

---
+++ SIMServer/SIMServer.Backlog.cs
@@ -0,0 +1,23 @@
+﻿namespace SIMServer
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class Backlog
+    {
+        public Backlog()
+        {
+            this.Messages = new Dictionary<int, List<SIMCommon.Message>>();
+        }
+
+        public Backlog(Dictionary<int, List<SIMCommon.Message>> messages)
+        {
+            this.Messages = messages;
+        }
+
+        public Dictionary<int, List<SIMCommon.Message>> Messages { get; private set; }
+    }
+}
