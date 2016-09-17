4740871e0fcd86f90e5677d6723319d708e95f3f
Sty BL <stysdaman@gmail.com>
2016-09-17 11:24:04 +0300

    Implemented SIMClient TcpClient class.

---
+++ SIMClient/SIMClient.TcpClient.cs
@@ -0,0 +1,46 @@
+﻿namespace SIMClient
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Net;
+    using System.Net.Sockets;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public static class TcpClient
+    {
+        public static string Send(string address, string message)
+        {
+            try
+            {
+                var client = new System.Net.Sockets.TcpClient(address, SIMCommon.Constants.SIMServerPort);
+                var stream = client.GetStream();
+
+                byte[] data = System.Text.Encoding.ASCII.GetBytes(message);
+                stream.Write(data, 0, data.Length);
+
+                data = new byte[SIMCommon.Constants.TcpStreamByteSize];
+                string response = string.Empty;
+                int bytes = stream.Read(data, 0, data.Length);
+                response = System.Text.Encoding.ASCII.GetString(data, 0, bytes);
+
+                stream.Close();
+                client.Close();
+
+                return response;
+            }
+            catch (Exception e) when (e is SocketException || e is System.IO.IOException)
+            {
+                throw new ConnectionFailiureException();
+            }
+        }
+
+        public class ConnectionFailiureException : Exception
+        {
+            public ConnectionFailiureException()
+            {
+            }
+        }
+    }
+}
