0f2afbe0678fea2d0adc3de88ad9585e6bef1a7e
Sty BL <stysdaman@gmail.com>
2016-09-18 18:59:07 +0300

    Implemented MsgDB class, WriteMessage and ReadThread methods.

---
+++ SIMClient/SIMClient.MsgDB.cs
@@ -0,0 +1,52 @@
+﻿namespace SIMClient
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Data.SQLite;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using SDatabase.SQLite;
+
+    public class MsgDB
+    {
+        public MsgDB()
+        {
+            this.ConnectionString = new ConnectionString(SIMCommon.Constants.SIMClientDatabaseFilename, 3);
+            this.Connection = new SQLiteConnection(this.ConnectionString.Text);
+        }
+
+        private ConnectionString ConnectionString { get; set; }
+
+        private SQLiteConnection Connection { get; set; }
+
+        public void WriteMessage(SIMCommon.Message message)
+        {
+            SDatabase.SQLite.Convert.SerializeObject(this.Connection, message, SIMCommon.Constants.SIMClientDatabaseMessageTable);
+        }
+
+        public List<SIMCommon.Message> ReadThread(int threadID, int offset = 0)
+        {
+            var result = new List<SIMCommon.Message>();
+
+            using (var cmd = new SQLiteCommand("SELECT  FROM @table WHERE threadID = @id ORDER BY timestamp DEC LIMIT @limit OFFSET @offset;", this.Connection))
+            {
+                cmd.Prepare();
+                cmd.Parameters.AddWithValue("@table", SIMCommon.Constants.SIMClientDatabaseMessageTable);
+                cmd.Parameters.AddWithValue("@id", threadID);
+                cmd.Parameters.AddWithValue("@limit", SIMCommon.Constants.SIMClientMsgLoadLimit);
+                cmd.Parameters.AddWithValue("@offset", offset);
+
+                using (var reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        result.Add(new SIMCommon.Message(reader.GetInt32(0), reader.GetInt32(1), reader.GetString(2), reader.GetDateTime(3)));
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+}
--- SIMCommon/SIMCommon.Constants.cs
+++ SIMCommon/SIMCommon.Constants.cs
@@ -5,6 +5,9 @@
         public const int TcpStreamByteSize = 512;
         public const int SIMServerPort = 13197;
         public const int SIMClientPort = 71913;
+        public const int SIMClientMsgLoadLimit = 50;
+        public const string SIMClientDatabaseFilename = "MsgDB.db";
+        public const string SIMClientDatabaseMessageTable = "SIMMESSAGES";
         public const string SIMAuthDBUserTable = "SIMUSERS";
         public const string SIMServerConfigFilename = "SIMServer.cfg";
         public const string SIMServerInvalidRequestResponse = "NULL";
