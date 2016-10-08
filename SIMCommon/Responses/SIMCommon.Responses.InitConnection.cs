515f9584b4332a6619c6b4bb8315bbe700e399e0
Sty <Sty@STYLAPTOP>
2016-10-08 10:48:12 +0300

    Modified InitConnection response to have a success property.

--- SIMCommon/Responses/SIMCommon.Responses.InitConnection.cs
+++ SIMCommon/Responses/SIMCommon.Responses.InitConnection.cs
@@ -2,11 +2,22 @@
 {
     public sealed class InitConnection : Base
     {
-        public InitConnection(string publicKey, int leaseDuration) : base()
+        public InitConnection(bool success)
         {
+            this.Success = success;
+            this.PublicKey = null;
+            this.LeaseDuration = -1;
+        }
+
+        [Newtonsoft.Json.JsonConstructor]
+        public InitConnection(bool success, string publicKey, int leaseDuration) : base()
+        {
+            this.Success = success;
             this.PublicKey = publicKey;
             this.LeaseDuration = leaseDuration;
         }
+
+        public bool Success { get; private set; }
 
         public string PublicKey { get; private set; }
 
